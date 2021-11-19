b07da1f4370eaf0eb886ff46481e94843885ee60
EGiets <153030@ettyhillesumlyceum.nl>
2021-11-19 21:24:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ygcs7x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ygcs7x)
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ],
+})
+export class AppComponent  {
+  name = 'Ellis ' ;
+  input: string;
+  output: string;
+
+  verplaats () {
+    this.output = this.input;
+    this.input = '';
+  }
+}
